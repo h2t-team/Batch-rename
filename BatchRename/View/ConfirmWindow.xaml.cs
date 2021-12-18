6382c1f773aef0e3a87e7040b5edc7912214baac
Huy Doan <huy11900@gmail.com>
2021-12-18 12:37:51 +0700

    Improve 11

---
+++ BatchRename/View/ConfirmWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BatchRename.View
+{
+    /// <summary>
+    /// Interaction logic for ConfirmWindow.xaml
+    /// </summary>
+    public partial class ConfirmWindow : Window
+    {
+        public string Message { get; set; }
+        public ConfirmWindow(string msg)
+        {
+            InitializeComponent();
+            Message = msg;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+        }
+
+        private void Confirm_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
